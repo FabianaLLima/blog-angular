3d2a17167f26ab51f80b183691d7b0ab2579351f
FabianaLLima <fabianallima@hotmail.com>
2025-03-28 21:32:04 -0300

    Creating the component base and the title menu

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,10 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { Component } from '@angular/core';
 
-export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
-];
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-blog';
+}
