15d8ce520d730866f870e75f413cd380c8eb0ec2
FabianaLLima <fabianallima@hotmail.com>
2025-04-10 23:01:21 -0300

    componets dinamycs propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent implements OnInit
-{
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover: string = ""
+  @Input()
+  cardTitle:string = ""
   constructor() { }
 
   ngOnInit(): void {
